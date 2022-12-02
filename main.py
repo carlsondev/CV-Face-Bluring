d58eea7ae152c3d47452c0df3588797f6bedd441
Benjamin Carlson <bcarlson336@gmail.com>
2022-12-01 22:56:51 -0700

    Implemented face blurring and detection

--- main.py
+++ main.py
@@ -4,85 +4,42 @@ import numpy as np
 import cv2
 import imutils
 import time
-from typing import Tuple, List, Any
 
-NMS_THRESHOLD = 0.3
-MIN_CONFIDENCE = 0.2
+from typing import List, Tuple
 
-labels_path = "yolov4_dependencies/coco.names"
-weights_path = "yolov4_dependencies/yolov4-tiny.weights"
-config_path = "yolov4_dependencies/yolov4-tiny.cfg"
-
-person_labels_idx = 0
-
-face_cascade = cv2.CascadeClassifier(
-    cv2.data.haarcascades + "haarcascade_frontalface_alt.xml"
-)
+from yolo_person_detection import get_person_rects, RectType
+from face_detection import get_face_rects, blur_faces
 
 
-def setup_model() -> Tuple[Any, List[str]]:
-    model = cv2.dnn.readNetFromDarknet(config_path, weights_path)
+def draw_rects(
+    bgr_img: np.ndarray, body_rects: List[RectType], face_rects: List[RectType]
+) -> np.ndarray:
 
-    layer_name = model.getLayerNames()
-    layer_name = [layer_name[i - 1] for i in model.getUnconnectedOutLayers()]
+    img = bgr_img.copy()
 
-    return model, layer_name
+    for body in body_rects:
+        x, y, w, h = body
+
+        img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    for face in face_rects:
+        face_x, face_y, face_w, face_h = face
+        img = cv2.circle(
+            img,
+            (int(face_x + face_w // 2), int(face_y + face_h // 2)),
+            10,
+            (0, 0, 255),
+            -1,
+        )
+
+    return img
 
 
-model, layer_name = setup_model()
+def get_minutes_seconds(seconds: float) -> Tuple[int, int]:
+    time_min = int(seconds // 60)
+    time_s = int(seconds - (60 * time_min))
 
-
-def get_person_rects(
-    image: np.ndarray,
-) -> List[Tuple[Tuple[float, float], Tuple[float, float]]]:
-    h, w = image.shape[:2]
-
-    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
-    model.setInput(blob)
-    layerOutputs = model.forward(layer_name)
-
-    boxes = []
-    confidences = []
-
-    for output in layerOutputs:
-        for detection in output:
-
-            scores = detection[5:]
-            classID = np.argmax(scores)
-            confidence = scores[classID]
-
-            if classID != person_labels_idx:
-                continue
-
-            if confidence <= MIN_CONFIDENCE:
-                continue
-
-            box = detection[0:4] * np.array([w, h, w, h])
-            center_x, center_y, width, height = box.astype("int")
-
-            x = int(center_x - (width / 2))
-            y = int(center_y - (height / 2))
-
-            boxes.append([x, y, int(width), int(height)])
-            confidences.append(float(confidence))
-
-    # apply non-maxima suppression to suppress weak, overlapping
-    # bounding boxes
-    idxs = cv2.dnn.NMSBoxes(boxes, confidences, MIN_CONFIDENCE, NMS_THRESHOLD)
-    # ensure at least one detection exists
-
-    if len(idxs) <= 0:
-        return []
-
-    rects = []
-
-    # loop over the indexes we are keeping
-    for i in idxs.flatten():
-        # extract the bounding box coordinates
-        x, y, w, h = boxes[i]
-        rects.append(((x, y), (x + w, y + h)))  # Top left and bottom right coordinates
-    # return the list of results
-    return rects
+    return time_min, time_s
 
 
 def main():
@@ -92,6 +49,13 @@ def main():
     )  # Open video capture object
     got_image, bgr_img = video_capture.read()  # Make sure we can read video
 
+    img_h, img_w, _ = bgr_img.shape
+
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    fps = video_capture.get(cv2.CAP_PROP_FPS)
+    frame_count = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    out = cv2.VideoWriter("out_small.mp4", fourcc, fps, (img_w, img_h))
+
     if not got_image:
         print("Cannot read video source")
         exit(1)
@@ -100,44 +64,51 @@ def main():
     cv2.namedWindow(window_name)
     window_is_open = True
 
+    current_frame_num = 1
+    program_start_time = time.time()
+    frame_comp_times: List[float] = []
+
     while window_is_open and got_image:
         start_time = time.time()
         bgr_img = imutils.resize(bgr_img, width=700)
-        gray_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2GRAY)
-        boxes = get_person_rects(bgr_img)
+        print(f"Currently processing frame {current_frame_num}/{frame_count}")
+        body_rects = get_person_rects(bgr_img)
 
-        faces = face_cascade.detectMultiScale(gray_img, 1.1, 4)
+        face_rects = get_face_rects(bgr_img)
+        print(f"Faces Size: {len(face_rects)}")
+        # bgr_img = draw_rects(bgr_img, body_rects, face_rects)
+        bgr_img = blur_faces(bgr_img, body_rects, face_rects)
+        end_time = time.time()
 
-        if len(faces) > 0:
-            print(len(faces))
-            for face in faces:
-                face_x, face_y, face_w, face_h = face
-                # face_bl = (
-                #     box_tl[0] + face_x,
-                #     box_tl[1] + face_y,
-                # )  # Transform smaller coordinate space to real image space
-                cv2.circle(
-                    bgr_img,
-                    (face_x + face_w // 2, face_y + face_h // 2),
-                    20,
-                    (0, 0, 255),
-                    -1,
-                )
+        extra_start = time.time()
+        # Compute the average computation time of every frame
+        frame_comp_time = end_time - start_time
+        frame_comp_times.append(frame_comp_time)
 
-        for box in boxes:
-            box_tl, box_br = box
+        frame_comp_time_avg = sum(frame_comp_times) / len(frame_comp_times)
 
-            # person_img = gray_img[
-            #     box_tl[1] : box_br[1],
-            #     box_tl[0] : box_br[0],
-            # ]
+        # Predict the amount of time remaining
+        predicted_seconds_remaining = (
+            frame_count - current_frame_num
+        ) * frame_comp_time_avg
 
-            cv2.rectangle(bgr_img, box_tl, box_br, (0, 255, 0), 2)
+        pred_min, pred_s = get_minutes_seconds(predicted_seconds_remaining)
 
-        print(f"Frame took {time.time() - start_time:.2f} seconds")
+        # Get the total time the program has taken
+        exec_time = end_time - program_start_time
+        exec_time_min, exec_time_s = get_minutes_seconds(exec_time)
 
-        cv2.imshow(window_name, bgr_img)
+        print(f"Frame #{current_frame_num} took {frame_comp_time:.2f} seconds")
+        print(
+            f"Program execution time: {exec_time_min}:{exec_time_s:02}/{pred_min}:{pred_s:02}"
+        )
 
+        frame_comp_times[-1] += time.time() - extra_start
+        print("-" * 50)
+
+        # cv2.imshow(window_name, bgr_img)
+        out.write(bgr_img)
+        current_frame_num += 1
         got_image, bgr_img = video_capture.read()
 
         key = cv2.waitKey(1)
@@ -145,6 +116,7 @@ def main():
             break
 
     video_capture.release()
+    out.release()
     cv2.destroyAllWindows()
 
 
